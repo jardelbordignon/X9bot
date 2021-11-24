b3b5dfcd27effe6504758261e27a1dc83c25b735
Jardel <jardel1101@gmail.com>
2021-11-24 16:08:20 -0300

    lesson 1

---
+++ README.md
@@ -0,0 +1,37 @@
+Parte 1 https://luiztools.com.br/x9-1
+
+*Códigos retornados da Binance*
+---
+* o - Preço de abertura da vela
+* c - Preço de fechamento da vela ou preço atual
+* h - Preço máximo da vela
+* l - Preço mínimo da vela
+* v - Volume da Vela
+* t - Tempo da vela em milisegundos
+* q - Quantidade de negociações na vela
+* O - Preço de abertura da vela no último minuto
+* C - Preço de fechamento da vela no último minuto
+* H - Preço máximo da vela no último minuto
+* L - Preço mínimo da vela no último minuto
+* V - Volume da Vela no último minuto
+* Q - Quantidade de negociações na vela no último minuto
+* P - Preço de compra mais alto da vela
+* p - Preço de venda mais baixo da vela
+* B - Volume de negociações no último minuto
+* A - Volume de negociações no último minuto
+* b - Volume de negociações no último minuto
+* a - Volume de negociações no último minuto
+* n - Número de velas na sessão
+* s - Status da sessão
+* i - Índice da Sessão
+* f - Fechamento da sessão
+* L - Volume total da sessão
+* l - Volume total da sessão
+* T - Tempo da sessão em milisegundos
+* Q - Quantidade de negociações na sessão
+* q - Quantidade de negociações na sessão
+* V - Volume total da sessão
+* v - Volume total da sessão
+* D - Volume total da sessão
+* d - Volume total da sessão
+---
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,54 @@
 import express from 'express'
 import 'dotenv/config'
+import WS from 'ws'
+
+interface ITradeOperation {
+  amount: number
+  type: 'purchase' | 'sale'
+  currentPrice: number
+  date?: Date
+}
+
+const binance_ws_uri = 'wss://stream.binance.com:9443/ws'
+const currency = 'ethbusd'
+const period = '1h'
+
+const ws = new WS(`${binance_ws_uri}/${currency}@kline_${period}`)
+
+const tradeOperations: ITradeOperation[] = []
+
+const getBalance = () => {
+  const balance = { purchase: 0, sale: 0, total: 0 }
+
+  tradeOperations.forEach(operation => balance[operation.type] += operation.amount)
+
+  balance.total = balance.purchase - balance.sale
+
+  return balance
+}
+
+const tradeOperation = ({amount, type, currentPrice}: ITradeOperation): void => {
+  tradeOperations.push({ amount, type, currentPrice, date: new Date() })
+  console.log(tradeOperations)
+}
+
+ws.onmessage = async (event: any): Promise<void> => {
+  const data = JSON.parse(event.data)
+  const currentPrice = parseFloat(data.k.c)
+  const highPrice = parseFloat(data.k.h)
+  const lowPrice = parseFloat(data.k.l)
+
+  console.log(`Valor atual: ${currentPrice}`)
+  console.log(`Menor valor em ${period}: ${lowPrice}`)
+  console.log(`Maior valor em ${period}: ${highPrice}`)
+
+  if (currentPrice >= highPrice) {
+    const amount = getBalance().total
+    tradeOperation({ amount, currentPrice, type: 'sale'})
+  } else if (currentPrice < lowPrice) {
+    tradeOperation({ amount: 1000, currentPrice, type: 'purchase'})
+  }
+}
 
 const app = express()
 
